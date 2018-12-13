07ca5f7434476cadbcd1062a48c39db6c3de7a78
Lautaro <lautaronudel@gmail.com>
2018-12-13 17:38:57 -0300

    Lab07 terminado

--- src/AppBanco.js.js
+++ src/AppBanco.js.js
@@ -5,23 +5,92 @@ export default class AppBanco extends Component {
     constructor(props) {
         super(props);
         this.state = {
+			email: '',
+			cuit: '',
             moneda: 1,
-            capitalInicial: 0,
-            capitalFinal: 0,
+            capitalInicial: 0.00,
+            capitalFinal: 0.00,
+			dias: 10,
+			condiciones: false,
+			tasa: 0.0,
+			intereses: 0.00,
+			
         };
-        this.hacerPlazoFijo = this.hacerPlazoFijo.bind(this);
+		rtdo="";
+       this.hacerPlazoFijo = this.hacerPlazoFijo.bind(this);
+	   this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
     }
+	
+	forceUpdateHandler(){
+		this.forceUpdate();
+	}
+	
     hacerPlazoFijo() {
-        ToastAndroid.show('Presiono el boton de hacer plazo fijo!', ToastAndroid.LONG);
+		if(this.state.email== '' || this.state.cuit=='' || this.state.capitalInicial==''){
+			alert("Error\nComplete todos los campos");
+		}else{
+			if(this.state.condiciones){
+				rtdo="Capital Inicial: "+this.state.capitalInicial+"\nIntereses: "+this.state.intereses+"\nCapital Final: "+this.state.capitalFinal+"\nDias: "+this.state.dias;
+				this.forceUpdateHandler();
+				ToastAndroid.show('Plazo fijo exitoso', ToastAndroid.LONG);
+			}else{
+				rtdo="";
+				this.forceUpdateHandler();
+				alert("Error\nDebe aceptar los terminos y condiciones");
+			}
+		}
     }
+	
+	calcularTasa(){
+		if(this.state.dias<30 && this.state.capitalInicial<=5000){
+				this.setState({tasa: 25.0});
+			}
+			else if(this.state.dias>=30 && this.state.capitalInicial<=5000){
+				this.setState({tasa: 27.5});
+			}
+			else if(this.state.dias<30 && this.state.capitalInicial>5000 && this.state.capitalInicial<=99999){
+				this.setState({tasa: 30.0});
+			}
+			else if(this.state.dias>=30 &&  this.state.capitalInicial>5000 && this.state.capitalInicial<=99999){
+				this.setState({tasa: 32.3});
+			}else if(this.state.dias<30 &&  this.state.capitalInicial>99999) {
+				this.setState({tasa: 35.0});
+			} else this.setState({tasa: 38.5});
+	}
+	
+	intereses(){
+		this.setState({intereses: (this.state.capitalInicial*( Math.pow(1+this.state.tasa/100,this.state.dias/360)-1)).toFixed(2)});
+		this.setState({capitalFinal: (parseFloat(this.state.capitalInicial)+parseFloat(this.state.intereses)).toFixed(2)});
+	}
+	
+	actualizarDatos(valor){
+		this.setState({ dias: Math.round(valor)});
+		this.calcularTasa();
+		this.intereses();
+	}
+	
 	
     render() {
         return (
             <View style={styles.container}>
                 <Text>Correo Electronico</Text>
-                <TextInput>correo@mail.com</TextInput>
+                <TextInput
+				style=
+				{{
+				height: 35,width:200, borderColor: 'gray', borderWidth: 1
+				}}
+				keyboardType="email-address"
+				onChangeText={(valor) => this.setState({email: valor})}
+				></TextInput>
                 <Text>CUIT</Text>
-                <TextInput>00-00000000-0</TextInput>
+                <TextInput
+				style=
+				{{
+				height: 35,width:200, borderColor: 'gray', borderWidth: 1
+				}}
+				keyboardType="numeric"
+				onChangeText={(valor) => this.setState({cuit: valor})}
+				></TextInput>
                 <Text>Moneda</Text>
                 <Picker
                     style={{ width: 200 }}
@@ -31,18 +100,35 @@ export default class AppBanco extends Component {
                     <Picker.Item label="Pesos ARS" value="2" />
                 </Picker>
                 <Text>Monto</Text>
-                <TextInput>000</TextInput>
+                <TextInput
+				style=
+				{{
+				height: 35,width:200, borderColor: 'gray', borderWidth: 1
+				}}
+				keyboardType="decimal-pad"
+				onChangeText={(valor) => this.setState({capitalInicial: valor})}>
+				</TextInput>
                 <Text>Dias</Text>
-                <Slider></Slider>
-                <Text>10 dias</Text>
+                <Slider
+					maximumValue={180}
+					minimumValue={10}
+					style={{ width: 350 }}
+					onValueChange={(valor) => this.actualizarDatos(valor)}>
+				</Slider>
+                <Text>{this.state.dias} dias</Text>
+				<Text>${this.state.intereses}</Text>
+				<Text>Avisar por mail</Text>
                 <Switch></Switch>
-                <Text>Avisar por mail</Text>
-                <CheckBox title='Acepto condiciones' />
+                <Text>Aceptar Terminos y Condiciones</Text>
+                <CheckBox
+				value={this.state.condiciones}
+				title='Acepto condiciones'
+				onChange={() => this.setState({ condiciones: !this.state.condiciones })}/>
                 <Button title="Hacer Plazo Fijo"
                     color="#FF0000"
                     onPress={this.hacerPlazoFijo}>
                 </Button>
-                <Text>[[ RESULTADO DE LA OPERACION ]]</Text>
+				<Text>{rtdo}</Text>
             </View>
         );
     }
@@ -53,7 +139,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'flex-start',
-        alignItems: 'flex-start',
+        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
     welcome: {
